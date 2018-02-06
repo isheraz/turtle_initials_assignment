0900d5e9910d2facc5b23c9b5b82e6c942599d75
Sheraz Ahmed <sherazahmed93@gmail.com>
2018-02-07 02:37:19 +0500

    Made Letter S

--- Letters.py
+++ Letters.py
@@ -1,5 +1,38 @@
-class Drawable_Alphabets:
+import turtle
 
-    def: draw_A(letter):
 
-    def: draw_S(letter):
+class DrawableAlphabets:
+    def draw_a(letter):
+        letter.right(45)
+        letter.backward(200)
+
+    def draw_s(letter):
+        letter.forward(100)
+        letter.left(90)
+        letter.forward(100)
+        letter.left(90)
+        letter.forward(100)
+        letter.left(90)
+        letter.backward(100)
+        letter.left(90)
+        letter.forward(100)
+
+    def draw_art(self, character):
+        window = turtle.Screen()
+        window.bgcolor("#000000")
+
+        aplhabet = turtle.Turtle()
+        aplhabet.shape('turtle')
+        aplhabet.speed(1)
+        aplhabet.color('#fff')
+
+        if character == 'a':
+            DrawableAlphabets.draw_a(aplhabet)
+        if character == 's':
+            DrawableAlphabets.draw_s(aplhabet)
+        window.exitonclick()
+
+
+initials = DrawableAlphabets()
+initials.draw_art('s')
+initials.draw_art('a')
